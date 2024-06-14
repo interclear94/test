d0df9dd4a41b1552e542f3ed677256765e9ea3d4
interclear94 <https://github.com/interclear94>
2024-06-14 16:58:34 +0900

    테스트

---
+++ function.js
@@ -0,0 +1,19 @@
+// Function Declaration
+function num1(n){
+  return n;
+}
+
+console.log(num1(2));
+
+
+// Function Expression
+const fn1 = function fn1(){
+  console.log(3);
+}
+fn1()
+
+// ArrowFunction
+const fn2 = () => {
+  console.log(4)
+}
+fn2()
---
+++ test2.js
@@ -0,0 +1,16 @@
+// function isPrime(n) {
+//   let num = 1;
+//   if(n > 1){
+//     while(num == n){
+//       console.log(n % num)
+//       num++;
+//       // continue;
+//     }
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(isPrime(31));
+// console.log(isPrime(10));
